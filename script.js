44545d84ddb5a72d0132b0a445e4474efc71e6aa
mythicoco <talavy201@email.com>
2025-10-12 09:51:35 +0300

    added wet sand

--- script.js
+++ script.js
@@ -7,15 +7,16 @@ document.querySelectorAll('.cube').forEach(cube => {cube.addEventListener('mouse
 const sandColor = 'yellow'
 const waterColor = 'lightBlue'
 const stealColor = 'gray'
+const wetSand = 'rgb(139, 128, 0)'
 let currentColor = sandColor
-async function draw(a) {const classNum = Number(a.target.classList[0].match(/\d+/g));;document.querySelector(`.c${classNum}`).style.backgroundColor = currentColor;}
+async function draw(a) {const classNum = Number(a.target.classList[0].match(/\d+/g));document.querySelector(`.c${classNum}`).style.backgroundColor = currentColor;}
 
 let physics = false
 async function handleClick() {
     physics = physics ? false : true
     physics ? document.querySelector('.idk').innerHTML = 'click me<br>to turn off physics' : document.querySelector('.idk').innerHTML = 'click me<br>to turn on physics'
     while (physics) {let board = getBoard()
-        for (let i=board.length-index-1;i>=0;i--) {if (board[i] === 1 || board[i] === 2) {board = lower(i, board)}}
+        for (let i=board.length-1;i>=0;i--) {if (board[i] === 1 || board[i] === 2 || board[i] === 5) {board = lower(i, board)}}
         placeBoard(board)
         await new Promise(x => setTimeout(x,1))}
 }
@@ -24,16 +25,13 @@ function setElementAs(element) {if (element === 1) {currentColor = sandColor} el
 
 function lower(i, board) {
     const r = (i / index) | 0, c = i % index
-    if (board[i] === 1) {if (board[i+index] === 0) {board[i] = 0;board[i+index] = 1} else {if (board[i+index-1] !== 0 && isEmptyFor(i,1,board) && board[i+index+1] === 0 && c !== index-1) {;board[i] = 0;board[i+index+1] = 1} else if (board[i+index-1] === 0 && isEmptyFor(i,-1,board) && board[i+index+1] !== 0 && c !== 0) {board[i] = 0;board[i+index-1] = 1} else if ((board[i+index-1] === 0 && isEmptyFor(i, -1, board) && c !== 0) || (board[i+index+1] === 0 &&  isEmptyFor(i, 1, board) && c !== index-1)) {if (isEmptyFor(i,-1,board)) {board[i] = 0;board[i+index-1] = 1} else if (isEmptyFor(i,1,board)) {board[i] = 0;board[i+index+1] = 1}} else if (board[i+index-1] === 1 && board[i+index+1] === 1) return board;}
-    } else if (board[i] === 2) {if (board[i+index] === 0) {board[i] = 0;board[i+index] = 2; return board;}
-        for(let x=1;x<=50;x++) {
-            if (board[i+index-x] === 0 && board[i+index+x] !== 0 && c >= x && isEmptyFor(i, -x, board)) {board[i] = 0;board[i+index-x] = 2;return board} else if (board[i+index-x] !== 0 && board[i+index+x] === 0 && c <= index-(x+1) && isEmptyFor(i, x, board)) {board[i] = 0;board[i+index+x] = 2;return board}
-            else if ((board[i+index-x] === 0 &&  isEmptyFor(i, -x, board) &&  c >= x) || (board[i+index+x] === 0 && c <= index-(x+1) && isEmptyFor(i, x, board))) {if(isEmptyFor(i,-x,board)) {board[i] = 0;board[i+index-x] = 2;return board} else if (isEmptyFor(i,x,board)) {board[i] = 0;board[i+index+x] =2;return board}}
-                else if (board[i+index+x] === 0 && c <= index-1-x && isEmptyFor(i, x, board)) {board[i] = 0;board[i+index+x] = 2;return board} else if (board[i+index-x] === 0 && c >= x && isEmptyFor(i, -x, board)) {board[i] = 0;board[i+index-x] = 2;return board}
-        }}; return board
+    if (board[i] === 1) {if (board[i+index] === 0) {board[i] = 0;board[i+index] = 1; return board} else if (board[i+index] === 2) {board[i] = 2; board[i+index] = 1; return board} else {if (board[i+index-1] !== 0 && isEmptyFor(i,1,board) && board[i+index+1] === 0 && c !== index-1) {board[i] = 0;board[i+index+1] = 1} else if (board[i+index-1] === 0 && isEmptyFor(i,-1,board) && board[i+index+1] !== 0 && c !== 0) {board[i] = 0;board[i+index-1] = 1} else if ((board[i+index-1] === 0 && isEmptyFor(i, -1, board) && r !== index-1 && c !== 0) || (board[i+index+1] === 0 &&  isEmptyFor(i, 1, board) && r !== index-1 && c !== index-1)) {if (isEmptyFor(i,-1,board) && c !== 0) {board[i] = 0;board[i+index-1] = 1} else if (isEmptyFor(i,1,board) && c !== index-1) {board[i] = 0;board[i+index+1] = 1}} else if (board[i-1] === 2 || board[i-index] === 2 || board[i+1] === 2 || board[i+index] === 2 && c !== 0 && c !== 0) {board[i] = 5} return board;}}
+    else if (board[i] === 2) {if (board[i+index] === 0) {board[i] = 0;board[i+index] = 2; return board;}
+        for(let x=1;x<=50;x++) {if (board[i+index-x] === 0 || board[i+index+x] === 0) {if(isEmptyFor(i,-x,board) && c >= x && board[i+index-x] === 0) {board[i] = 0;board[i+index-x] = 2;return board} else if (isEmptyFor(i,x,board) && c <= index-x-1 && board[i+index+x] === 0) {board[i] = 0;board[i+index+x] = 2;return board}}}} else if (board[i] === 5) {
+    if (board[i+index] === 2) {board[i] = 2; board[i+index] = 5} else if (board[i+index] === 0) {board[i] = 0;board[i+index] = 5}else if (board[i+index-1] === 2 && board[i-1] === 2 && board[i+index+1] === 2 && board[i+1] === 2 && c !== 0 && c !== index-1) {const num = Math.round(Math.random() + 1) === 1 ? 1 : -1; board[i] =2;board[i+index+num] = 5} else if (board[i+index-1] === 2 && board[i-1] === 2 && c !== 0) {board[i] = 2;board[i+index-1] = 5} else if (board[i+index+1] === 2 && board[i+1] === 2 && c !== index-1) {board[i] = 2;board[i+index+1] = 5} else if (board[i-1] !== 2 && board[i-index] !== 2 && board[i+1] !== 2 && board[i+index] !== 2) {board[i] = 1}} return board
 }
 
 function isEmptyFor(i, x, board) {for(let a=1;a<=Math.abs(x);a++) {if(board[i+ ((x > 0) ? a : (a * -1))] !== 0) return false}return true}
 
-function placeBoard(b) {b.forEach((cube, i) => {if (cube === 0) {document.querySelector(`.c${i}`).style.backgroundColor = 'white'} else if (cube === 1) {document.querySelector(`.c${i}`).style.backgroundColor = sandColor} else if (cube === 2) {document.querySelector(`.c${i}`).style.backgroundColor = waterColor} else if (cube === 3) {document.querySelector(`.c${i}`).style.backgroundColor = stealColor}})}
-function getBoard() {let board = [];document.querySelectorAll('.cube').forEach(cube => {if (getComputedStyle(cube).backgroundColor === 'rgb(255, 255, 255)') {board.push(0)} else if (getComputedStyle(cube).backgroundColor === 'rgb(255, 255, 0)') {board.push(1)} else if (getComputedStyle(cube).backgroundColor === 'rgb(173, 216, 230)') {board.push(2)} else if (getComputedStyle(cube).backgroundColor === 'rgb(128, 128, 128)') {board.push(3)}});return board;}
+function placeBoard(b) {b.forEach((cube, i) => {if (cube === 0) {document.querySelector(`.c${i}`).style.backgroundColor = 'white'} else if (cube === 1) {document.querySelector(`.c${i}`).style.backgroundColor = sandColor} else if (cube === 2) {document.querySelector(`.c${i}`).style.backgroundColor = waterColor} else if (cube === 3) {document.querySelector(`.c${i}`).style.backgroundColor = stealColor} else if (cube === 5) {document.querySelector(`.c${i}`).style.backgroundColor = wetSand}})}
+function getBoard() {let board = [];document.querySelectorAll('.cube').forEach(cube => {if (getComputedStyle(cube).backgroundColor === 'rgb(255, 255, 255)') {board.push(0)} else if (getComputedStyle(cube).backgroundColor === 'rgb(255, 255, 0)') {board.push(1)} else if (getComputedStyle(cube).backgroundColor === 'rgb(173, 216, 230)') {board.push(2)} else if (getComputedStyle(cube).backgroundColor === 'rgb(128, 128, 128)') {board.push(3)} else if (getComputedStyle(cube).backgroundColor === wetSand) {board.push(5)}});return board;}
